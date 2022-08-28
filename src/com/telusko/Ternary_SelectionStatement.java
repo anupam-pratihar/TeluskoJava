17d2a613a4053bb21fff7b71e2dc4b7b42d9bd67
Ginura Ransika <ginuraransika@gmail.com>
2022-08-28 19:29:06 +0530

    added new informations about the ternary operator

--- src/com/telusko/Ternary_SelectionStatement.java
+++ src/com/telusko/Ternary_SelectionStatement.java
@@ -1,5 +1,7 @@
 package com.telusko;
 
+import java.util.Scanner;
+
 public class Ternary_SelectionStatement {
     public static void main(String[] args) {
 
@@ -17,20 +19,61 @@ public class Ternary_SelectionStatement {
         System.out.println(j);
 
 
-// Ternary Method
-        // with the ternary type we can replace this code with one line
-        // Ternary Operator
-        // ?: condition?expression1:expression2
-        // if the condition is true it will execute the expression 1
-        // if it is false it will execute the expression 2
+        /*
+         ! Ternary Method
+            with the ternary type we can replace this code with one line
+         * Ternary Operator
+            *  ?: condition?expression1:expression2
+          if the condition is true it will execute the expression 1
+          if it is false it will execute the expression 2
+         */
         int a = 9;
         int b = 0;
+
         // I want to assign the value of b
         // if a is greater than 8 , 5 will assign to the b
         // if a is lower than 8 ( condition is false ) , 10 will assign to the b
-        b =  a > 8? 5: 10;
+
+        b =  a > 8 ? 5: 10;
         System.out.println(b);
-        
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter number count: ");
+        int count = scanner.nextInt();
+        while(count != 0)
+        {
+            System.out.print("Enter your number  : ");
+            int number = scanner.nextInt();
+            // ! Variable = Condition? Expression1:Expression2
+            String answer = number % 2 == 1 ? "Odd": "Even";
+            count--;
+            System.out.println(number + " is an " + answer);
+        }
+
+        System.out.print("Enter a boolean : ");
+        boolean trueOrFalse = scanner.nextBoolean();
+        Object obj1;
+        if(trueOrFalse)
+            obj1 = new Integer(10);
+        else
+            obj1 = new Double(15.6);
+
+        System.out.println(obj1);
+
+
+        System.out.print("Enter a boolean 2 : ");
+        boolean trueOrFalse2= scanner.nextBoolean();
+        Object obj2;
+
+        //* even if it's true this will print 10.0
+        /*
+        ! because ternary operator will check type of the both the values and whichever
+        ! is maximum it will occupy that type
+        ! Hence, by default obj2 will be double
+        */
+
+        obj2=trueOrFalse2?new Integer(10):new Double(15.6);
+        System.out.println(obj2);
 
     }
 }
